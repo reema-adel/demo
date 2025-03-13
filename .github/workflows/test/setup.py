ab01b2804693153974aeac95b0e18fbc83cecbde
reema-adel <85634408+reema-adel@users.noreply.github.com>
2025-03-13 14:25:50 +0300

    Update setup.py

--- .github/workflows/test/setup.py
+++ .github/workflows/test/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
   name="my_project",
-  version="0.1",
-  packages=find_packages(),
-  install_requires=[
-    "pytest",
-  ],
+   version="0.1",
+   packages=find_packages(),
+   install_requires=[
+     "pytest",
+   ],
 )
